5b75847f2327bbe4a650d83bc13771ded082d5e3
jainabhishek0406@gmail.com <jainabhishek0406@gmail.com>
2019-11-14 01:18:52 +0530

    sample code with implementation of String immutability

--- src/ImmutableStringEx.java
+++ src/ImmutableStringEx.java
@@ -8,16 +8,22 @@ public class ImmutableStringEx {
         String str1 = "Abhishek";
         String str2 = "Jain";
 
-        System.out.println("str1 & str2 values before string concatenation");
-        System.out.println("str1 = " + str1);
-        System.out.println("str2 = " + str2);
+        System.out.println("str1 & str2 values and hashcode before string concatenation");
+        System.out.println("str1 hashcode = " + str1.hashCode());
+        System.out.println("str1 content = " + str1);
+        System.out.println("str2 hashcode = " + str2.hashCode());
+        System.out.println("str2 content = " + str2);
 
         //string concatenation
         str1 = str1+str2;
 
-        System.out.println("\nstr1 & str2 values before string concatenation");
-        System.out.println("str1 = " + str1);
-        System.out.println("str2 = " + str2);
+        System.out.println("\nstr1 & str2 values and hashcode after string concatenation");
+        System.out.println("str1 hashcode = " + str1.hashCode());
+        System.out.println("str1 content = " + str1);
+        System.out.println("str2 hashcode = " + str2.hashCode());
+        System.out.println("str2 content = " + str2);
 
+        //str1 hashcode is changed after concatenation, it means reference is updated because object is created again after concatenation.
+        //str2 hashcode is same.
     }
 }
